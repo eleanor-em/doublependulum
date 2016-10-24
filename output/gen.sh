45b94368b3c262c41d97d230ee06da92e92bdd65
Eleanor McMurtry <elmcmurtry1@gmail.com>
2016-10-24 23:22:47 +1100

    tab to space

--- output/gen.sh
+++ output/gen.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo '#!/bin/bash'													>> run.sh
+echo '#!/bin/bash'                                                  >> run.sh
 # resize the images
 for i in `seq 0 499`;
 do
-	echo 'convert pgplot'$i'.png -resize 680x680\! pgplot'$i'.png'	>> run.sh
+    echo 'convert pgplot'$i'.png -resize 680x680\! pgplot'$i'.png'  >> run.sh
 done
-echo 'echo "images resized..."'										>> run.sh
+echo 'echo "images resized..."'                                     >> run.sh
 # list all files in order to be added to the gif
-echo -n 'convert -loop 0 '											>> run.sh
+echo -n 'convert -loop 0 '                                          >> run.sh
 for i in `seq 0 499`;
 do
-	echo -n pgplot$i.png' '											>> run.sh
+    echo -n pgplot$i.png' '                                         >> run.sh
 done
-echo 'pendulum.gif'													>> run.sh
-echo 'echo "done!"'													>> run.sh
+echo 'pendulum.gif'                                                 >> run.sh
+echo 'echo "done!"'                                                 >> run.sh
 echo "starting..."
 ./run.sh
 # clean up temporary files
